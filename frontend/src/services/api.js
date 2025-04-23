8a90cc4ef25e132ca5383ac1a72dc802c9465adf
ankitranjan1028 <aankitrn17@gmail.com>
2025-04-23 10:48:40 +0530

    server.js minor fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,32 +1,12 @@
-// import axios from 'axios';
-
-// const API_URL = import.meta.env.VITE_API_URL;
-
-
-// const api = {
-//   getContacts: async () => {
-//     try {
-//       const response = await axios.get(`${API_URL}/contacts`);
-//       return response.data.contacts;
-//     } catch (error) {
-//       console.error('Error fetching contacts:', error);
-//       throw error;
-//     }
-//   }
-// };
-
-// export default api;
-
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+
 const api = {
   getContacts: async () => {
     try {
-      const response = await axios.get(`${API_URL}/contacts`, {
-        withCredentials: true, 
-      });
+      const response = await axios.get(`${API_URL}/contacts`);
       return response.data.contacts;
     } catch (error) {
       console.error('Error fetching contacts:', error);
@@ -36,3 +16,4 @@ const api = {
 };
 
 export default api;
+
